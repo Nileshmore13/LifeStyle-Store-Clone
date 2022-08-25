4957b9449a49749301c16b74120df5fd1aa51285
Nileshrmore13 <Nileshrmore13@gmail.com>
2022-08-25 23:29:05 +0530

    day2

---
+++ womens_wear/mens.js
@@ -0,0 +1,198 @@
+let mens = [
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010840277-Beige-Beige-1000010840277_01-2100.jpg",
+      "title": "U.S. POLO ASSN. Men Printed Casual Shirt",
+      "color": "Beige",
+      "offer": "40%",
+      "price": "₹ 1379",
+      "id": 14
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010314158-Blue-Blue-1000010314158_01-2100.jpg",
+      "title": "U.S. POLO ASSN. Men Solid Casual Shirt",
+      "color": "Blue",
+      "offer": "50%",
+      "price": "₹ 1349",
+      "id": 15
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011365455-White-White-1000011365455_01-2100.jpg",
+      "title": "BOSSINI Men Printed Slim Casual Shirt",
+      "color": "White",
+      "offer": "50%",
+      "price": "₹ 749",
+      "id": 16
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010750972-Blue-Navy-1000010750972_01-2100.jpg",
+      "title": "FAME FOREVER Men Checked Slim Fit Casual Shirt",
+      "color": "Blue",
+      "offer": "50%",
+      "price": "₹ 849",
+      "id": 17
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011277396-Brown-Brown-1000011277396_01-2100.jpg",
+      "title": "CODE Men Printed Slim Fit Casual Shirt",
+      "color": "Brown",
+      "offer": "40%",
+      "price": "₹ 779",
+      "id": 18
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011031588-Yellow-Yellow-1000011031588_01-2100.jpg",
+      "title": "BOSSINI Men Checked Slim Casual Shirt",
+      "color": "Yellow",
+      "offer": "50%",
+      "price": "₹ 649",
+      "id": 19
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011146281-Red-Red-1000011146281_01-2100.jpg",
+      "title": "BOSSINI Men Checked Spread Collar Casual Shirt",
+      "color": "Red",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 20
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011146294-Black-Black-1000011146294_01-2100.jpg",
+      "title": "BOSSINI Men Checked Regular Fit Casual Shirt",
+      "color": "₹ 779",
+      "offer": "40%",
+      "price": "",
+      "id": 21
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010435340-Black-Black-1000010435340_01-2100.jpg",
+      "title": "FORCA Men Solid Denim Casual Shirt",
+      "color": "Black",
+      "offer": "50%",
+      "price": "₹ 999",
+      "id": 22
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011178295-Blue-IceBlue-1000011178295_01-2100.jpg",
+      "title": "BOSSINI Men Solid Regular Fit Casual Shirt",
+      "color": "Blue",
+      "offer": "50%",
+      "price": "₹ 849",
+      "id": 23
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010920908-Red-1000010920908_01-2100.jpg",
+      "title": "BOSSINI Men Checked Regular Fit Casual Shirt",
+      "color": "Red",
+      "offer": "30%",
+      "price": "₹ 1189",
+      "id": 24
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011146336-White-White-1000011146336_01-2100.jpg",
+      "title": "FAME FOREVER Men Printed Slim Fit Casual Shirt",
+      "color": "White",
+      "offer": "40%",
+      "price": "₹ 479",
+      "id": 25
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011146268-White-White-1000011146268_01-2100.jpg",
+      "title": "BOSSINI Men Checked Regular Fit Casual Shirt",
+      "color": "White",
+      "offer": "White",
+      "price": "₹ 779",
+      "id": 26
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011148311-Blue-LightBlue-1000011148311_01-2100.jpg",
+      "title": "FAME FOREVER Men Checked Slim Fit Casual Shirt",
+      "color": "Blue",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 27
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010919468-Blue-Blue-1000010919468_01-2100.jpg",
+      "title": "FORCA Men Striped Casual Shirt",
+      "color": "Blue",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 28
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011148296-Pink-LightPink-1000011148296_01-2100.jpg",
+      "title": "FAME FOREVER Men Checked Slim Fit Casual Shirt",
+      "color": "Pink",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 29
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011148252-White-White-1000011148252_01-2100.jpg",
+      "title": "FAME FOREVER Men Checked Slim Casual Shirt",
+      "color": "White",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 30
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010230981-Green-Green-1000010230981_01-2100.jpg",
+      "title": "U.S. POLO ASSN. Men Checked Regular Fit Casual Shirt",
+      "color": "Green",
+      "offer": "50%",
+      "price": "₹ 1249",
+      "id": 31
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010905815-Purple-Purple-1000010905815_01-2100.jpg",
+      "title": "JACK & JONES Men Printed Slim Casual Shirt",
+      "color": "Purple",
+      "offer": "40%",
+      "price": "₹ 1379",
+      "id": 32
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011277396-Green-Green-1000011277396_01-2100.jpg",
+      "title": "CODE Men Printed Slim Fit Casual Shirt",
+      "color": "Green",
+      "offer": "30%",
+      "price": "₹ 909",
+      "id": 33
+    },
+    {
+      "image": "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011514917-Blue-Navy-1000011514917_01-2100.jpg",
+      "title": "FORCA Men Checked Regular Fit Casual Shirt",
+      "color": "Blue",
+      "offer": "10%",
+      "price": "₹ 899",
+      "id": 34
+    }
+  ]
+
+
+ 
+
+  ShowProduct(mens)
+function ShowProduct(data) {
+    
+    let product = document.getElementById("products");
+    product.innerHTML = null;
+
+    data.forEach(function (el, i) {
+        let div = document.createElement("div");
+        div.setAttribute("class","product_details");
+
+        let image = document.createElement("img");
+        image.src = el.image;
+
+        let price = document.createElement("h4");
+        price.innerText = el.price;
+
+        let title = document.createElement("p");
+        title.innerText = el.title;
+
+        div.append(image, price, title);
+
+        product.append(div);
+    });
+}
